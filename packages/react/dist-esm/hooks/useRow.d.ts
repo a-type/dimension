5ced4a321f9b3001a96346bba5c6fa7b90c1e26f
Grant Forrest <a-type@outlook.com>
2020-03-22 17:22:30 -0400

    remove dist-esm folder

--- packages/react/dist-esm/hooks/useRow.d.ts
+++
@@ -1,6 +0,0 @@
-import { ROW_CONTAINER_ATTRIBUTE } from '@dimension/core';
-export declare const useRow: () => {
-    props: {
-        [ROW_CONTAINER_ATTRIBUTE]: boolean;
-    };
-};
--- packages/react/dist-esm/internal/hooks.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const useIsMounted: () => boolean;
-export declare const usePrevious: <T>(val: T) => T;
-export declare const useIdOrGenerated: (providedId?: string | undefined, idBase?: string | undefined) => string;
-export declare function useControlled<T>({ controlled, default: defaultProp, name, onChange, }: {
-    controlled?: T;
-    default: T;
-    name?: string;
-    onChange?: (val: T) => void;
-}): [T, (val: T) => void];
