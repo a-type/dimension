da786dc9345713bf10053e6fa4e6f0f9826416ad
Grant Forrest <gaforres@gmail.com>
2020-05-03 18:51:19 -0400

    trying netlify...

---
+++ netlify-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yarn workspace @dimension/core build:cjs
+yarn workspace @dimension/core build:esm
+yarn workspace @dimension/dom build:cjs
+yarn workspace @dimension/dom build:esm
+yarn workspace @dimension/react build:cjs
+yarn workspace @dimension/react build:esm
+rm -rf docs
+mkdir docs
+yarn workspace @dimension/homepage build:gh
+cp -a ./packages/homepage/dist/. ./docs
+cp -a ./packages/homepage/docs/. ./docs/docs
